e8fbdcbb1dde7fea9bc87a2b79c49dc042d5482e
Richard-Persson <ricpersson00@gmail.com>
2024-11-12 12:27:14 +0100

    laget enkelt terreng

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
 
 
 window.onload = function init(){
@@ -8,7 +9,13 @@ window.onload = function init(){
   const renderer = new THREE.WebGLRenderer()
   document.body.appendChild(renderer.domElement)
 
+  //Laste inn bilder
+  const loader = new THREE.TextureLoader()
+  const texture = loader.load('./static/terrain.png')
+  const height = loader.load('./static/heightMap.png')
 
+  //GUI
+  const gui = new dat.GUI()
 
   //Lager scenen
   const scene = new THREE.Scene()
@@ -20,10 +27,10 @@ window.onload = function init(){
   )
 
   //Setter posisjonen
-  camera.position.set(0,2,5)
+  camera.position.set(0,3,4)
 
   //Lager akse og grid for hjelp
-  const gridHelper = new THREE.GridHelper()
+  const gridHelper = new THREE.GridHelper(10)
   scene.add(gridHelper)
   const axesHelper = new THREE.AxesHelper(5)
   scene.add(axesHelper)
@@ -32,18 +39,43 @@ window.onload = function init(){
   const orbit = new OrbitControls(camera,renderer.domElement) 
   scene.add(orbit)
 
-  //Cube
-  const boxGeometry = new THREE.BoxGeometry()
-  const boxMaterial = new THREE.MeshBasicMaterial({color:0xFFFF})
-  const box = new THREE.Mesh(boxGeometry,boxMaterial)
-  scene.add(box)
+
+  //Lys
+  const pointLight = new THREE.PointLight(0xffffff,2)
+  scene.add(pointLight)
+  pointLight.position.x = 1
+  pointLight.position.y = 4
+  pointLight.position.z = 1
+
+
+  //Sphere
+  const sphereGeometry = new THREE.SphereGeometry()
+  const sphereMaterial = new THREE.MeshPhongMaterial({color:0x98765})
+  const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial)
+  scene.add(sphere)
+  sphere.position.y = 3
+  sphere.position.x = 3
+
+  
+  
+  //Plane
+  const planeGeometry = new THREE.PlaneGeometry(5,5,64,64)
+  const planeMaterial = new THREE.MeshStandardMaterial({
+    map:texture,
+    displacementMap:height,
+  })
+  const plane = new THREE.Mesh(planeGeometry,planeMaterial)
+  scene.add(plane)
+  plane.rotation.x = -0.5*Math.PI 
+
+
 
 
   //Animerer scenen
   function animate(time) {
 
-    box.rotation.x = time/1000
-    box.rotation.y= time/1000
+    sphere.rotation.x = time/1000
+    sphere.rotation.y = time/1000
 
     renderer.render(scene,camera)
   }
